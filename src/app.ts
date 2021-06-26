9b0aa7fb3248fe49a53e398cb234c067e1f1bade
Tom Brown <thomas.brown-cic-uk@ibm.com>
2021-06-26 16:31:11 +0100

    add intial submit handler

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -15,8 +18,24 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title')!;
+        this.descriptionInputElement = this.element.querySelector('#description')!;
+        this.peopleInputElement = this.element.querySelector('#people')!;
         
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+        console.log(this.descriptionInputElement.value);
+        console.log(this.peopleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     private attach() {
