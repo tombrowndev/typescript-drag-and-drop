90a0f8e1f3cd9c7cbdd2d28d81513a2a6fb3f00c
Tom Brown <thomas.brown-cic-uk@ibm.com>
2021-06-26 16:11:11 +0100

    create basic project input class

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,12 @@
  * Later, it will be separated up as we learn more about modules in TypeScript.
  */
 
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    }
+}
