82b63b14625cffcbb9ee339ecb8fd6952707ea5d
Tom Brown <thomas.brown-cic-uk@ibm.com>
2021-06-26 15:52:08 +0100

    add comment regarding codebase

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+/** 
+ * For now, all code will be written in 1 file as per the course recommendation. 
+ * Later, it will be separated up as we learn more about modules in TypeScript.
+ */
+
