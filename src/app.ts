370cfb5859d2dfdf6e987c9d20a2ea03af86f460
Tom Brown <thomas.brown-cic-uk@ibm.com>
2021-06-27 16:21:03 +0100

    add validation to user input

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,41 @@
  * Later, it will be separated up as we learn more about modules in TypeScript.
  */
 
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(input: Validatable) {
+  let isValid = true;
+
+  if (input.required) {
+    isValid = isValid && input.value.toString().trim().length !== 0;
+  }
+
+  if (input.minLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.length >= input.minLength;
+  }
+
+  if (input.maxLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.length <= input.maxLength;
+  }
+
+  if (input.min != null && typeof input.value === "number") {
+    isValid = isValid && input.value >= input.min;
+  }
+
+  if (input.max != null && typeof input.value === "number") {
+    isValid = isValid && input.value <= input.max;
+  }
+
+  return isValid;
+}
+
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
@@ -44,28 +79,56 @@ class ProjectInput {
     this.attach();
   }
 
-  private gatherUserInput(): [string, string, number] {
+  private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    // TODO: Add input validation
-    return [enteredTitle, enteredDescription, +enteredPeople];
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Input invalid, please try again");
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
   }
 
   private clearInputs() {
-      this.titleInputElement.value = ''
-      this.descriptionInputElement.value = ''
-      this.peopleInputElement.value = ''
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
-    const [title, desc, people] = userInput;
-    console.log(title, desc, people);
-    this.clearInputs();
+
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
