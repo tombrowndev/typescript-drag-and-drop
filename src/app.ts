7e96b8049469751f3519ad0abefd96fb0795de09
Tom Brown <thomas.brown-cic-uk@ibm.com>
2021-06-27 15:42:09 +0100

    create autobind descriptor

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,68 @@
-/** 
- * For now, all code will be written in 1 file as per the course recommendation. 
+/**
+ * For now, all code will be written in 1 file as per the course recommendation.
  * Later, it will be separated up as we learn more about modules in TypeScript.
  */
 
+function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
 class ProjectInput {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    element: HTMLFormElement;
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    peopleInputElement: HTMLInputElement;
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
-    constructor() {
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input';
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
 
-        this.titleInputElement = this.element.querySelector('#title')!;
-        this.descriptionInputElement = this.element.querySelector('#description')!;
-        this.peopleInputElement = this.element.querySelector('#people')!;
-        
-        this.configure();
-        this.attach();
-    }
+    this.titleInputElement = this.element.querySelector("#title")!;
+    this.descriptionInputElement = this.element.querySelector("#description")!;
+    this.peopleInputElement = this.element.querySelector("#people")!;
 
-    private submitHandler(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
-        console.log(this.descriptionInputElement.value);
-        console.log(this.peopleInputElement.value);
-    }
+    this.configure();
+    this.attach();
+  }
 
-    private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this))
-    }
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    console.log(this.descriptionInputElement.value);
+    console.log(this.peopleInputElement.value);
+  }
 
-    private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
-    }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
 }
 
 const projectInput = new ProjectInput();
-
